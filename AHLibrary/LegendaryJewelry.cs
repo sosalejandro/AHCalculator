869172907c4430da40119d3e1950cbe740c10e4f
Alejandro Sosa <wata-fock_alejandro@hotmail.com>
2021-01-25 10:09:24 -0400

    second change
    abstracting data, making it more general purpose than limited to a single class. Prior to update the library with more libraries

--- AHLibrary/LegendaryJewelry.cs
+++ AHLibrary/LegendaryJewelry.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using static System.Console;
+using System.Collections.Generic;
 
 namespace AHLibrary
 {
     public class ShadowghastRing
     {
         public int Rank;
-        public RingMats Mats;
+        public Dictionary<string, int> materials;
         public ShadowghastRing(int rank)
         {
             Rank = rank;
@@ -17,71 +18,37 @@ namespace AHLibrary
                 2 => 15,
                 _ => 10
             };
-            Mats = new RingMats(amountNeeded);
+            
+            materials = new Dictionary<string, int>();
+            materials.Add("Shadowghast Ingot", amountNeeded * 2);
+            materials.Add("Essence of Rebirth", amountNeeded);
+            materials.Add("Essence of Torment", amountNeeded);
+            materials.Add("Orboreal Shard", amountNeeded);
             WriteLine(format: "Created an instance of ring {0}", arg0: rank);
         }
         public void MatsNeeded()
         {
-            WriteLine("{0,-8} {1,16}", "Material", "Amount");
-            WriteLine("{0,-8} {1,7}", nameof(Mats.ShadowghastIngot), Mats.ShadowghastIngot);
-            WriteLine("{0,-8} {1,7}", nameof(Mats.EssenceOfRebirth), Mats.EssenceOfRebirth);
-            WriteLine("{0,-8} {1,7}", nameof(Mats.EssenceOfTorment), Mats.EssenceOfTorment);
-            WriteLine("{0,-8} {1,10}", nameof(Mats.OrborealShard), Mats.OrborealShard);
+            WriteLine("{0,-8} {1,25}", "Material", "Amount");
+            foreach (KeyValuePair<string, int> item in materials)
+            {
+                WriteLine("{0,-20}\t {1,9}", item.Key, item.Value);
+            }
         }
 
         public void CostToCraft()
         {
-            decimal mat1;
-            decimal mat2;
-            decimal mat3;
-            decimal mat4;
-
-            bool input1;
-            bool input2;
-            bool input3;
-            bool input4;
-
-            Write($"What's the cost of the Shadowghast Ingots? price a piece: ");
-            input1 = decimal.TryParse(ReadLine(), out mat1);
-            decimal mat1cost = mat1 * Mats.ShadowghastIngot;
-            WriteLine($"It will cost {mat1cost} gold.");
-
-            
-            Write($"What's the cost of the Essence of Rebirth? price a piece: ");
-            input2 = decimal.TryParse(ReadLine(), out mat2);
-            decimal mat2cost = mat2 * Mats.EssenceOfRebirth;
-            WriteLine($"It will cost {mat2cost} gold.");
-            
-            
-            Write($"What's the cost of the Essence of Torment? price a piece: ");
-            input3 = decimal.TryParse(ReadLine(), out mat3);
-            decimal mat3cost = mat3 * Mats.EssenceOfTorment;
-            WriteLine($"It will cost {mat3cost} gold.");
-
-            
-            Write($"What's the cost of the Orboreal Shard? price a piece: ");
-            input4 = decimal.TryParse(ReadLine(), out mat4);
-            decimal mat4cost = mat4 * Mats.OrborealShard;
-            WriteLine($"It will cost {mat4cost} gold.");
-
-            decimal totalCost = mat1cost + mat2cost + mat3cost + mat4cost;
-            WriteLine($"It's total cost will be a total cost of {totalCost} gold.");
-        }
-    }
-
-    public class RingMats
-    {
-        public int ShadowghastIngot;
-        public int EssenceOfTorment;
-        public int EssenceOfRebirth;
-        public int OrborealShard;
-
-        public RingMats(int amount)
-        {
-            ShadowghastIngot = amount * 2;
-            EssenceOfTorment = amount;
-            EssenceOfRebirth = amount;
-            OrborealShard = amount;
+            Dictionary<string, decimal> Cost = new Dictionary<string, decimal>();
+            decimal totalCost = 0m;
+            foreach (KeyValuePair<string, int> item in materials)
+            {
+                decimal currentPrice;
+                Write("What's the current price for {0}?: ", item.Key);
+                bool input = decimal.TryParse(ReadLine(), out currentPrice);
+                Cost.Add(item.Key, currentPrice);
+                WriteLine("It will cost {0}", item.Value * currentPrice);
+                totalCost += item.Value * currentPrice;
+            }
+            WriteLine("Total cost will be {0}", totalCost);
         }
     }
 }
--- JewelCrafting/Program.cs
+++ JewelCrafting/Program.cs
@@ -6,25 +6,38 @@ namespace JewelCrafting
 {
     class Program
     {
+        public static void CraftingItem(string crafting)
+        {
+            var Crafting = crafting switch
+            {
+                string r => new ShadowghastRing(ItemRank()),
+                _ => null
+            };
+            Crafting.MatsNeeded();
+            Crafting.CostToCraft();
+        }
+
+        public static int ItemRank()
+        {
+            Write("Please, enter the desired rank of the item: ");
+            bool inputRank = int.TryParse(ReadLine(), out int RANK);
+            return RANK;
+        }
+
+        public static void Start()
+        {
+            WriteLine("This is the legendary crafting helper, what will you craft today?\n" +
+                   "Ring: R\t Necklace: N");
+            Write("Enter the letter for desired item to craft: ");
+            string input = ReadLine().ToLower();
+            CraftingItem(input);
+            WriteLine("The program will exit, press enter to shutdown.");
+            ReadLine();
+        }
+
         static void Main(string[] args)
         {
-            // ShadowghastRing RingTest = new ShadowghastRing(4);
-            // RingTest.MatsNeeded();
-            // RingTest.CostToCraft();
-
-            WriteLine("This is the ring crafting price helper, what will you craft today?");
-            Write("Ring Rank: ");
-            bool input1 = int.TryParse(ReadLine(), out int rank);
-            ShadowghastRing RingToCraft = new ShadowghastRing(rank);
-            
-            WriteLine("You will need the next amount: ");
-            RingToCraft.MatsNeeded();
-
-            Write("Press Enter to continue: ");
-            ReadLine();
-            RingToCraft.CostToCraft();
-            WriteLine("The program will exit, press enter to shutdown. ");
-            ReadLine();
+            Start();
         }
     }
 }
