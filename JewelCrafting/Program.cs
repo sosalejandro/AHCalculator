4f42ae7c88e8fb3dea897d9db602642763da5ce8
Alejandro Sosa <74624888+sosalejandro@users.noreply.github.com>
2021-02-07 21:00:31 -0400

    Update Program.cs

--- JewelCrafting/Program.cs
+++ JewelCrafting/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using static System.Console;
 using AHLibrary;
 
@@ -8,36 +8,49 @@ namespace JewelCrafting
     {
         public static void CraftingItem(string crafting)
         {
-            var Crafting = crafting switch
+            try
             {
-                string r => new ShadowghastRing(ItemRank()),
-                _ => null
-            };
-            Crafting.MatsNeeded();
-            Crafting.CostToCraft();
+                var Crafting = crafting switch
+                {
+                    string r when r.Contains('r') => new ShadowghastRing(ItemRank()),
+                    _ => null
+                };   
+                Crafting.MatsNeeded();
+                Crafting.CostToCraft();             
+            }
+            catch(NullReferenceException ex)
+            {
+                WriteLine(ex);
+                Write("Enter a valid input\nRing: R\t Necklace: N");
+                Start();
+            }
         }
 
         public static int ItemRank()
         {
-            Write("Please, enter the desired rank of the item: ");
-            bool inputRank = int.TryParse(ReadLine(), out int RANK);
+            int RANK = 0;
+            while (RANK <= 0 || RANK >= 5)
+            {
+                Write("Please, enter a valid rank from 1 to 4: ");
+                bool inputRank = int.TryParse(ReadLine(), out RANK);
+            };
             return RANK;
         }
 
         public static void Start()
-        {
-            WriteLine("This is the legendary crafting helper, what will you craft today?\n" +
-                   "Ring: R\t Necklace: N");
-            Write("Enter the letter for desired item to craft: ");
+        {       
             string input = ReadLine().ToLower();
-            CraftingItem(input);
-            WriteLine("The program will exit, press enter to shutdown.");
-            ReadLine();
+            CraftingItem(input);            
         }
 
         static void Main(string[] args)
         {
+            WriteLine("This is the legendary crafting helper, what will you craft today?\nRing: R\t Necklace: N");
+            Write("Enter the letter for desired item to craft: ");
+
             Start();
+            WriteLine("The program will exit, press enter to shutdown.");
+            ReadLine();
         }
     }
 }
